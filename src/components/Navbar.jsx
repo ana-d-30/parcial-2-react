afdcd605afd8720a369532129a977ee1eedd0d28
ana-d-30 <anahi.flores@davinci.edu.ar>
2026-07-03 18:11:24 -0300

    Diseñar estilos principales

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import "./../styles/navbar.css";
+
 
 const Navbar = () => {
     return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,33 @@
+import { Link } from "react-router-dom";
+import "../styles/home.css";
+
 const Home = () => {
     return (
-        <h1>Inicio</h1>
+        <section className="home">
+
+            <h1>Quimix</h1>
+
+            <h2>Sistema de Gestión de Insumos de Laboratorio</h2>
+
+            <p>
+                Bienvenido a Quimix. Esta aplicación permite administrar
+                productos e insumos de laboratorio de forma simple,
+                organizada y eficiente.
+            </p>
+
+            <div className="botones-home">
+
+                <Link to="/productos">
+                    <button>Ver Productos</button>
+                </Link>
+
+                <Link to="/login">
+                    <button>Iniciar Sesión</button>
+                </Link>
+
+            </div>
+
+        </section>
     );
 }
 
